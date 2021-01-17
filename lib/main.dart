d533910b9498ac5c06f3f0b8d8a7297b788b2f52
MostafaAzazy <mostafa3zazy@outlook.com>
2021-01-17 19:43:58 +0200

    h

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hvacr_tool_box/provider/auth_provider.dart';
-import 'package:hvacr_tool_box/screens/aboutUs.dart';
 import 'package:hvacr_tool_box/screens/myProfile.dart';
-import 'package:hvacr_tool_box/screens/privacyPolicy.dart';
-import 'package:hvacr_tool_box/screens/terms.dart';
 import 'package:provider/provider.dart';
-import 'screens/auth_screens/forget_password.dart';
-import 'screens/contact_us.dart';
 
 
 void main() {
