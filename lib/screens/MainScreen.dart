34b70608fad8234a78f56006775976cdbfbdd77c
MostafaAzazy <mostafa3zazy@outlook.com>
2021-01-21 23:54:29 +0200

    home

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,75 +1,108 @@
-
-
-
 import 'package:flutter/material.dart';
+import 'package:hvacr_tool_box/screens/compressor/Compressor.dart';
 import 'package:hvacr_tool_box/widgets/CategoryBar.dart';
 import 'package:hvacr_tool_box/widgets/Drawer.dart';
 import 'package:hvacr_tool_box/widgets/appBar.dart';
 
-class MainScreen extends StatefulWidget{
+import 'applications/application.dart';
+
+class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  String textlol = "3";
-  List<String> test = ["helllo" , "man" , "world"] ;
+  //String textlol = "3";
+  //List<String> test = ["helllo" , "man" , "world"] ;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Color(0xffffffff),
       bottomSheet: Container(
-        height:70,
-        width:MediaQuery.of(context).size.width,
+        height: 70,
+        width: MediaQuery.of(context).size.width,
         child: Image(image: AssetImage("images/group_2.png")),
       ),
-      appBar: appbar(context, " ", false , isdrawer: true , scaffoldKey:_scaffoldKey ) ,
-      drawer: drawer(context,),
+      appBar: appbar(context, " ", false,
+          isdrawer: true, scaffoldKey: _scaffoldKey),
+      drawer: drawer(
+        context,
+      ),
       body: ListView(
         children: [
-          mainCards( "images/home_images/compr.png" , "Compressors"),
-          mainCards( "images/home_images/app.png" , "Applications"),
-          mainCards( "images/home_images/torub.png" , "Troubleshooting"),
-          mainCards( "images/home_images/lib.png" , "Library"),
+          mainCards("images/home_images/compr.png", "Compressors" , 1),
+          mainCards("images/home_images/app.png", "Applications" , 2),
+          mainCards("images/home_images/torub.png", "Troubleshooting" , 3),
+          mainCards("images/home_images/lib.png", "Library" , 4),
+          SizedBox(height:70),
         ],
       ),
     );
   }
-  Widget mainCards(image , text){
-    return Container(
 
-      margin: EdgeInsets.all(20),
-      width: MediaQuery.of(context).size.width*.85,
-      height: MediaQuery.of(context).size.height*.18,
-       decoration: BoxDecoration(
-         image:  DecorationImage(
-           image: AssetImage(image),
-           fit: BoxFit.cover,
-         ),
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(7),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.grey
-            , blurRadius: 20,
-          )]
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            bottom: 10,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(text , style:TextStyle(color: Colors.black54 , fontSize:20 , fontWeight: FontWeight.w600) ),
-                Icon(Icons.arrow_forward)
-              ],
+  Widget mainCards(image, text , id) {
+    return GestureDetector(
+      onTap: (){
+           switch(id){
+             case 1 : Navigator.push(context, MaterialPageRoute(builder: (context)=>Compressor())); break;
+             case 2 :Navigator.push(context, MaterialPageRoute(builder: (context)=>Application())); break;
+             default:print("clicked");
+           } 
+          },
+          child: Container(
+        margin: EdgeInsets.all(15),
+        width: MediaQuery.of(context).size.width * .85,
+        height: 150,
+        decoration: BoxDecoration(
+          boxShadow: [
+                  BoxShadow(color: Colors.black26, blurRadius: 1)
+                ],
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: BoxFit.cover,
+          ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(7),
+        ),
+        child: Stack(
+          children: [
+            Opacity(
+              opacity: 0.65,
+                child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.white,
+                    Colors.black
+                  ]
+                  )
+                 ),),
             ),
-          )
-
-        ],
+            Container(
+              alignment: Alignment.bottomCenter,
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(text,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600)),
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/compressor/coolingCapacity.dart
+++ lib/screens/compressor/coolingCapacity.dart
@@ -5,31 +5,47 @@ import 'package:hvacr_tool_box/widgets/serchBar.dart';
 class CoolingCapacity extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height:MediaQuery.of(context).size.height*0.7,
-        padding: EdgeInsets.only(left:20 , right:20),
-        child: ListView(
-          children: [
-            serchBar((val){
-              print(val);
-            } , "Please enter compressor model"),
-            Container(
-              height:150,
-              width:MediaQuery.of(context).size.width,
-              color: Colors.black,
-            ),
-            Container(
-              height:150,
-              width:MediaQuery.of(context).size.width,
-              color: Colors.blue,
-            ),
-            Container(
-              height:150,
-              width:MediaQuery.of(context).size.width,
-              color: Colors.green,
-            ),
-          ],
-        ),
+    Map result={'model':'QA114P','name':'LG * R- 22* 220 V / 50 Hz * AC'};
+    return Column(
+      children: [
+       serchBar((value){},'please enter compressor model '),
+       Container(
+           padding: EdgeInsets.all(15),
+           width: MediaQuery.of(context).size.width * .9,
+           height: MediaQuery.of(context).size.height *.65,
+           margin: EdgeInsets.only(left: 10,right: 10),
+           decoration: BoxDecoration(
+             borderRadius: BorderRadius.circular(10),
+             color: Colors.white,
+             boxShadow: [
+               BoxShadow(
+                 color: Colors.grey.withOpacity(0.25),
+                 spreadRadius: 5,
+                 blurRadius: 7,
+                 offset: Offset(0, 3), // changes position of shadow
+               ),
+             ],
+           ),
+           child:ListView.builder(
+             itemCount: 10,
+             itemBuilder: (context, index) {
+               return Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Wrap(
+                     direction: Axis.vertical,
+                     spacing: 10,
+                     children: [
+                       Text('${result['model']}',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                       Text('${result['name']}',style: TextStyle(fontSize: 15,color:Colors.black45)),
+                     ],
+                   ),
+                   Divider(height: 22,),
+                 ],
+               );
+             },
+           ) )
+      ],
     );
   }
 }
